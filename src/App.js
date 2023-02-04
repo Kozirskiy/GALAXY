2804e49e101b9c2f143b26c3c96ca6032e1bbdbd
Olexandr <pelegrino@i.ua>
2023-02-04 14:58:07 +0200

    folders done

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 // import  React from 'react';
 import './App.css';
-import Header from './components/Header';
-import NavBar from './components/NavBar';
+import Header from './components/Header/Header';
+import NavBar from './components/Navbar/NavBar';
 
 
 const App = () => {
