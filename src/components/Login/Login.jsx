3870a06d5835e62062ae2a197ee85f93f8285595
Olexandr <pelegrino@i.ua>
2023-04-27 17:58:47 +0300

    75 less is done

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,23 +1,40 @@
 import React from "react";
 import s from './Login.module.css';
+import { reduxForm, Field } from 'redux-form';
 
 
-const Login = (props) => {
+const LoginForm = (props) => {
     return (
         <div className={s.contentBlock}>
             <h1>Please wrire your login here or exit from this site</h1>
-            <div className={s.wrapperLoginBox}>
-                <textarea name="" id="" cols="30" rows="10">
-                    Login
-                </textarea>
-                <textarea name="" id="" cols="30" rows="10">
-                    Password
-                </textarea>
-            </div>
-
+            <form onSubmit={props.handleSubmit} >
+                <div> 
+                    <Field placeholder={'Login' } name={'login'} component={'input'}/>
+                </div>
+                <div>
+                    <Field placeholder={'Password'} name={'password'} component={'input'}/>
+                </div>
+                <div>
+                    <Field component={'input'} name={'rememberMe'} type={'checkbox'}/> Remember me
+                </div>
+                <div>
+                    <button>Login</button>
+                </div>
+            </form>
         </div>
 
     )
 };
+const LoginReduxForm = reduxForm ({ form: 'login' })(LoginForm)
+ 
+const Login = (props) => {
+    const onSubmit = (formData) => {
+        console.log(formData)
+    }
+
+    return(
+        <LoginReduxForm onSubmit={onSubmit}/>
+    )
+}
 
 export default Login;
--- src/redux/redux-store.js
+++ src/redux/redux-store.js
@@ -4,12 +4,14 @@ import messageREDUCER from './messageREDUCER';
 import usersREDUCER from "./usersREDUCER";
 import authREDUCER from "./authREDUCER";
 import thunkMiddleware from 'redux-thunk';
+import { reducer as formReducer } from 'redux-form'
 
 let reducers = combineReducers({
     postPAGE: postREDUCER,
     messagePAGE: messageREDUCER,
     usersPAGE: usersREDUCER,
-    auth: authREDUCER
+    auth: authREDUCER,
+    form: formReducer
 });
 
 let store = legacy_createStore(reducers, applyMiddleware(thunkMiddleware));
